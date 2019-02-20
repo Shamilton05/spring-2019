f32c9201c39df2afa83cb4e9f23f465c01aff83a
Spencer Hamilton <34385199+Shamilton05@users.noreply.github.com>
2019-02-20 12:47:56 -0800

    Delete wordGrabber.h

--- wordGrabber.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-// Grabs a random word from a given text file with number of words in the first row and returns the length of the word
-int getWord(FILE *fp, char keyword[80]);
